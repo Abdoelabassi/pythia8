9da46fb5fdc71f5f8355562b8645142381e935d0
Abdoelabassi <abderrezaq@outlook.com>
2022-09-15 08:39:04 +0100

    updated tut4

--- tut4.cc
+++ tut4.cc
@@ -12,11 +12,11 @@ int main(){
 
 	TFile *output = new TFile("tut3.root","recreate");
 	TTree *tree =  new TTree("tree","tree");
-	
+
 	//vars to store in the ttree
 	int id, event, size, pn;
-	double mass, px, py, pz;
-	
+	double mass, px, py, pz, p;
+
 	tree->Branch("id",&id,"id/I");
 	tree->Branch("event",&event, "event/I");
 	tree->Branch("size",&size, "size/I");
@@ -25,6 +25,7 @@ int main(){
 	tree->Branch("px",&px,"px/D");
 	tree->Branch("py",&py,"py/D");
 	tree->Branch("pz",&pz,"pz/D");
+	tree->Branch("p",&p,"p/D");
 
 	int events = 1e4;
 	Pythia pythia;
@@ -34,9 +35,6 @@ int main(){
 	pythia.readString("SoftQCD:all= on");
 	pythia.readString("HardQCD:all= on");
 	pythia.init();
-	Hist hpz("Momementum Distribution in z direction",100,-10,10);
-	Hist hmass("Mass Distribution",50,0,5);
-
 	for (int i=0; i < events;i++){
 		if(!pythia.next()) continue;
 		int entries = pythia.event.size();
@@ -48,32 +46,17 @@ int main(){
 		for (int j=0; j < entries;j++){
 			id = pythia.event[j].id();
 			pn=j;
-			double mass = pythia.event[j].m();
-			double px = pythia.event[j].px();
-			double py = pythia.event[j].py();
-			double pz = pythia.event[j].pz();
+			mass = pythia.event[j].m();
+			px = pythia.event[j].px();
+			py = pythia.event[j].py();
+			pz = pythia.event[j].pz();
+			p = sqrt(pow(px,2) + pow(py,2) + pow(pz,2));
 			tree->Fill();
-			hpz.fill(pz);
-			hmass.fill(mass);
-
-			double p = sqrt(pow(px,2) + pow(py,2) + pow(pz,2));
 
 			std::cout << "id : " <<  id << " mass: " << mass << " Total momentum: " << p << std::endl;
 	 	}
 	}
 
-	 std::cout << hpz << std::endl;
-	 std::cout << hmass << std::endl;
-
-	 HistPlot hpl("tut2");
-	 HistPlot hpp ("tut2-2");
-    	 hpl.frame("pz", "Momentum Distribution", "Momentum", "Entries");
-	 hpp.frame("mass","Mass Distribution","Mass","Entries");
-    	 hpl.add(hpz);
-	 hpp.add(hmass);
-    	 hpl.plot();
-	 hpp.plot();
-	 
 	 output->Write();
 	 output->Close();
 
